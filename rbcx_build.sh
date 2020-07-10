c9220bc542a3fb87611215862110e5b2b703a9cd
Vojtěch Boček <vojtech.bocek@avast.com>
2020-07-10 23:58:31 +0200

    Add prebuilt bootloader for rbcx

--- rbcx_build.sh
+++ rbcx_build.sh
@@ -12,3 +12,8 @@ make \
     DFU_DNLOAD_NOSYNC=_DISABLE \
     DFU_DBLRESET_MS=_DISABLE \
     rbcx_v11
+
+if [ "$#" -ge "1" ] && [ "$1" = "pre" ]; then
+    mkdir -p prebuilt/rbcx_v11
+    cp -a build/firmware.* prebuilt/rbcx_v11/
+fi
